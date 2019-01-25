29edbe19c496643ffd0e9ba84ea2df923c59680e
rutvik pathak <rutvikui@gmail.com>
2019-01-25 18:28:46 -0500

    Updates

---
+++ precache-manifest.fb393be928671cb6ba2d811ceecaf284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9067dfa93ebbe1865ba9",
+    "url": "/wether/static/css/main.84adf3c4.chunk.css"
+  },
+  {
+    "revision": "9067dfa93ebbe1865ba9",
+    "url": "/wether/static/js/main.9067dfa9.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/wether/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "db45e5c3778a60e123fb",
+    "url": "/wether/static/js/runtime~main.db45e5c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wether/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7351a80c29a89f273453fe8a2f209d2c",
+    "url": "/wether/index.html"
+  }
+];
